4205bf30403c1f1aaf5b5da1b100e7b12a86aec3
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2012-02-16 17:47:33 -0600

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
 		name                 = "django-seo-cascade",
-		version              = "0.3",
+		version              = "0.4",
 		include_package_data = True,
 		packages             = find_packages(),
 		author               = "Lynn Dylan Hurley, Jack Shedd",
