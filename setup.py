8c6fcd8a37509b1c58386780022e76f3b1fee76f
Lynn Dylan Hurley <lynn.dylan.hurley@gmail.com>
2012-02-16 17:47:04 -0600

    add package manifest

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup, find_packages
 setup(
 		name                 = "django-seo-cascade",
 		version              = "0.3",
+		include_package_data = True,
 		packages             = find_packages(),
 		author               = "Lynn Dylan Hurley, Jack Shedd",
 		author_email         = "lynn.dylan.hurley@gmail.com",
 		description          = "Django SEO manager. Automatic sitemap generation, cascading meta tag blocks, and overrides via the django admin.",
 		url                  = "https://github.com/thisismess/django-seo-cascade",
-		include_package_data = True,
 		install_requires     = ['Django>=1.3.1', 'lxml>=2.3.3', 'ez_setup']
 )
